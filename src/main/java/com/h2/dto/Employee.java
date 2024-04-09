b809b2544e2b8bf3141d58c5d0969b366e17c265
Vignesh R <viggy22vs@gmail.com>
2024-04-09 19:36:14 +0530

    Added constructor with 2 variables to the Employee.

--- src/main/java/com/h2/dto/Employee.java
+++ src/main/java/com/h2/dto/Employee.java
@@ -23,6 +23,11 @@ public class Employee {
         this.email = email;
     }
 
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
     public int getId() {
         return id;
     }
