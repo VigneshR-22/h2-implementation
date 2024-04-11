1156356cbce36d6a31fc8227e85658c2e0548ef1
Vignesh R <viggy22vs@gmail.com>
2024-04-11 16:32:42 +0530

    Added Junit test for positive condition of GET employees.

---
+++ src/test/java/com/h2/controller/EmployeeControllerTest.java
@@ -0,0 +1,46 @@
+package com.h2.controller;
+
+import com.h2.dto.Employee;
+import com.h2.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeControllerTest {
+
+    @Mock
+    EmployeeRepository repository;
+
+    @InjectMocks
+    EmployeeController controller;
+
+    @Test
+    public void unitTest_getEmployees() {
+
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Rick");
+        employee.setEmail("rick@emp.com");
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee);
+        when(repository.findAll()).thenReturn(employeeList);
+        ResponseEntity<?> actualResponse = controller.getEmployees();
+        assertEquals(HttpStatus.OK, actualResponse.getStatusCode());
+        assertEquals(employeeList, actualResponse.getBody());
+
+    }
+
+}
